ecdd6ddbf72fb4f8937cbd727e792203287b5dc9
Lorenzo Cea <heavens_drive090@hotmail.com>
2023-05-04 21:24:37 -0300

    primer commit
    - Agregado ng-bootstrap
    - Integración con Firebase
    - Configuración de despliegue

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'sala-de-juegos-31e6d',
+    appId: '1:10086412169:web:bdcc5a3c5258b57cf37b58',
+    storageBucket: 'sala-de-juegos-31e6d.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDzzGrSf7qqjilCtAjx7GxExe7PtPqgT1c',
+    authDomain: 'sala-de-juegos-31e6d.firebaseapp.com',
+    messagingSenderId: '10086412169',
+    measurementId: 'G-FP40074DYT',
+  },};
